6b2205adf481d5508c95fa597436b22dd6e1b82f
Madelin FG <84932921+MadelinFg@users.noreply.github.com>
2024-06-19 13:48:51 -0600

    Add logo, Add Navbar component and Update index styles

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./Navbar.css";
+
+import logo from "../../assets/svg/logo-white.svg";
+
+const Navbar = () => {
+    return <div className="navbar">
+        <img src={logo} alt="PropDown logo" />
+
+        <ul className="menu-list">
+            <li>
+                <a className="menu-link" href="/">Home</a>
+            </li>
+            <li>
+                <a className="menu-link" href="#categories">Categories</a>
+            </li>
+            <li>
+                <a className="menu-link" href="#recommendations">Recommendations</a>
+            </li>
+            <li>
+                <a className="menu-link" href="#article">Articles</a>
+            </li>
+        </ul>
+    </div>;
+};
+
+export default Navbar;
