4ca9099fc8a8412285c821945d7cfb12c481f7e1
Madelin FG <84932921+MadelinFg@users.noreply.github.com>
2024-06-27 15:51:07 -0600

    Add categories icons, Add categories data file

---
+++ src/data/categories.js
@@ -0,0 +1,22 @@
+export const categories = [
+    {
+        id: 1,
+        name: 'House',
+        description: 'Lorem ipsum dolor sit amet consectetur. Ante pellentesque tristique et lobortis interdum mollis.',
+    },
+    {
+        id: 2,
+        name: 'Apartment',
+        description: 'Lorem ipsum dolor sit amet consectetur. Ante pellentesque tristique et lobortis interdum mollis.',
+    },
+    {
+        id: 3,
+        name: 'Home office',
+        description: 'Lorem ipsum dolor sit amet consectetur. Ante pellentesque tristique et lobortis interdum mollis.',
+    },
+    {
+        id: 4,
+        name: 'Warehouse',
+        description: 'Lorem ipsum dolor sit amet consectetur. Ante pellentesque tristique et lobortis interdum mollis.',
+    },
+]
